0ea788fd1b9b6c88d3b24c5953fdb5cac0cb4381
waltlhony <waltlhony@gmail.com>
2018-09-22 15:15:18 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/walthony/cursomc/domain/Cliente.java
+++ src/main/java/com/walthony/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -37,7 +38,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 
 	@JsonIgnore
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
